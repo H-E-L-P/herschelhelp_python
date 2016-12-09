808d30f0e5fda79bf0143a94d0ef780759850c49
Yannick Roehlly <yannick.roehlly@lam.fr>
2016-12-09 15:36:31 +0100

    Add mocfromdepth command

--- herschelhelp/commands.py
+++ herschelhelp/commands.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 import click
 from astropy.table import Table
 from pymoc import MOC
 
+from .depth_coverage import get_depth_coverage
 from .footprints import compute_coverages
 
 
@@ -40,3 +43,35 @@ def covbymoc(moc_file):
 
     print(output)
 
+
+@cli.command(short_help="Gets the MOC corresponding to depth criteria")
+@click.option('-k', default=None, type=float, help="K limiting magnitude.")
+@click.option('--output', default="coverage.fits",
+              help="Name of the output FITS file containing the MOC "\
+              "(coverage.fits by default).")
+def mocfromdepth(k, output):
+    """Produce the MOC corresponding to depth constraints.
+
+    Given some constraints on the depth of the data (see options below) this
+    command produce a Multi-Order Coverage map (MOC) of the corresponding area.
+    The programme connects to the HELP Virtual Observatory server to get the
+    information.
+
+    """
+
+    if os.path.isfile(output):
+        raise click.BadOptionUsage("The file {} already exists".format(output))
+
+    coverage_dict = {}
+
+    if k:
+        coverage_dict['K'] = k
+
+    if len(coverage_dict) == 0:
+        raise click.BadParameter("You must provide at least one depth limit. "
+                                 "See herschelhelp mocfromdepth --help.")
+    else:
+        result = get_depth_coverage(coverage_dict)
+        result.write(output)
+        print("The corresponding area was saved to {}. It covers {} square " \
+              "degrees.".format(output, result.area_sq_deg))
