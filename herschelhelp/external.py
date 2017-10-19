50f49a933c00233c085c606365f231c5c0e75bff
Yannick Roehlly <yannick.roehlly@lam.fr>
2017-10-19 18:55:04 +0100

    [tocigale] Set flux and error to NaN when flux=0 (optional)

--- herschelhelp/external.py
+++ herschelhelp/external.py
@@ -19,7 +19,7 @@ logging.basicConfig(level=logging.getLevelName(os.getenv('LOG_LEVEL', 'INFO')))
 LOGGER = logging.getLogger(__name__)
 
 
-def convert_table_for_cigale(catalogue, inplace=False):
+def convert_table_for_cigale(catalogue, inplace=False, remove_ulimits=True):
     """Convert a HELP formated catalogue to be used by CIGALE.
 
     This function converts a HELP formated catalogue to a CIGALE formated one:
@@ -30,6 +30,8 @@ def convert_table_for_cigale(catalogue, inplace=False):
     - The fluxes that are flagged not to be used in SED fitting as set to NaN;
     - For each band, for the sources at a redshift for which the Lyman limit
       interfere with the filter bandpass, the flux is set to NaN.
+    - If asked to remove upper limits, set to NaN flux and error for fluxes
+      equals to 0.
     - The fluxes are converted to mJy.
 
     Parameters
@@ -40,6 +42,9 @@ def convert_table_for_cigale(catalogue, inplace=False):
         If inplace is set to True, the function will not make a copy of the
         input catalogue.  This will save some memory space at the expense of
         modifying the input catalogue.
+    remove_ulimits: boolean
+        If remove_ulimits is True (default), all the 0 fluxes and associated
+        error are set to NaN.
 
     Returns
     -------
@@ -99,6 +104,14 @@ def convert_table_for_cigale(catalogue, inplace=False):
         else:
             LOGGER.warning("The catalogue is missing the ferr_%s column.",
                            band)
+
+        # Upper limits
+        if remove_ulimits:
+            mask = ~np.isnan(catalogue[band])
+            mask[mask] &= (catalogue[band][mask] == 0)
+            catalogue[band][mask] = np.nan
+            if '{}_err'.format(band) in catalogue.colnames:
+                catalogue['{}_err'.format(band)][mask] = np.nan
 
         # Set to NaN the fluxes in the filters that overlap or are below the
         # Lyman break limit at the source redshift.
