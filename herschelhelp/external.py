6740074948076a3b9cc373f451696d142c44aa0d
Yannick Roehlly <yannick.roehlly@lam.fr>
2017-10-20 11:26:50 +0100

    [tocigale] Make zero flux treatment optional
    And reword to use zerofluxes instead of upper limits as such fluxes
    wouldn't be considered as upper limits by CIGALE.

--- herschelhelp/external.py
+++ herschelhelp/external.py
@@ -19,7 +19,8 @@ logging.basicConfig(level=logging.getLevelName(os.getenv('LOG_LEVEL', 'INFO')))
 LOGGER = logging.getLogger(__name__)
 
 
-def convert_table_for_cigale(catalogue, inplace=False, remove_ulimits=True):
+def convert_table_for_cigale(catalogue, inplace=False,
+                             remove_zerofluxes=False):
     """Convert a HELP formated catalogue to be used by CIGALE.
 
     This function converts a HELP formated catalogue to a CIGALE formated one:
@@ -30,8 +31,8 @@ def convert_table_for_cigale(catalogue, inplace=False, remove_ulimits=True):
     - The fluxes that are flagged not to be used in SED fitting as set to NaN;
     - For each band, for the sources at a redshift for which the Lyman limit
       interfere with the filter bandpass, the flux is set to NaN.
-    - If asked to remove upper limits, set to NaN flux and error for fluxes
-      equals to 0.
+    - If asked to remove them, set to NaN flux and error for fluxes equals
+      to 0.
     - The fluxes are converted to mJy.
 
     Parameters
@@ -42,9 +43,8 @@ def convert_table_for_cigale(catalogue, inplace=False, remove_ulimits=True):
         If inplace is set to True, the function will not make a copy of the
         input catalogue.  This will save some memory space at the expense of
         modifying the input catalogue.
-    remove_ulimits: boolean
-        If remove_ulimits is True (default), all the 0 fluxes and associated
-        error are set to NaN.
+    remove_zerofluxes: boolean
+        If True, all the 0 fluxes and associated error are set to NaN.
 
     Returns
     -------
@@ -105,8 +105,8 @@ def convert_table_for_cigale(catalogue, inplace=False, remove_ulimits=True):
             LOGGER.warning("The catalogue is missing the ferr_%s column.",
                            band)
 
-        # Upper limits
-        if remove_ulimits:
+        # 0 fluxes
+        if remove_zerofluxes:
             mask = ~np.isnan(catalogue[band])
             mask[mask] &= (catalogue[band][mask] == 0)
             catalogue[band][mask] = np.nan
