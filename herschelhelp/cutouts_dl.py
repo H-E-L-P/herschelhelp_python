f6ee18465f5cc51eae78626ab8a1876e5cc6a7c0
Ian McCheyne <im281@apollo-login2.cm.cluster>
2019-12-09 16:00:09 +0000

    edited download panstarrs function toinclude a print statemet for debugging purposes

--- herschelhelp/cutouts_dl.py
+++ herschelhelp/cutouts_dl.py
@@ -832,7 +832,7 @@ def ps1_dl(ra, dec, band, radec_str, projcell=None, subcell=None, getFullIm=Fals
     if projcell == None:
         url_filenames = "http://ps1images.stsci.edu/cgi-bin/ps1filenames.py?ra=" + str(ra) + "&dec=" + str(dec)
         datas = urllib2.urlopen(url_filenames).readlines()
-        
+        print(datas)
         if len(datas) > 1:
             spilt_datas = datas[1].split(" ")
             projcell, subcell = spilt_datas[0], spilt_datas[1]
